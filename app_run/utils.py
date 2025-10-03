33e4a07c2db588a151dbac6133a8efbd10a7ce7a
mgorbatenko <mgorbatenko23@gmail.com>
2025-10-03 20:51:05 +0200

    solution task. convert deciaml to float in function utils.get_distance_to_object

--- app_run/utils.py
+++ app_run/utils.py
@@ -5,4 +5,4 @@ def get_distance_in_km(coordinates: list[tuple[float, float]]):
     return round(distance.distance(*coordinates).kilometers, 3)
 
 def get_distance_to_object(coordinates_1, coordinates_2):
-    return round(distance.distance(coordinates_1, coordinates_2).meters)
+    return round(distance.distance(coordinates_1, coordinates_2).meters, 3)
